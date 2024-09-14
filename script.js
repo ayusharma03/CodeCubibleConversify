8b9505fcab1169c494e3cc1aa751aa4917f667ab
Ayush Sharma <49690065+ayusharma03@users.noreply.github.com>
2024-09-15 02:10:53 +0530

    done dana done

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const blueText = document.querySelector('.blue');
-    const agentNameInput = document.getElementById('agentName');
-
-    blueText.addEventListener('click', function() {
-        agentNameInput.focus();
-    });
-});
